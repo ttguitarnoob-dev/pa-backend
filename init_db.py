45fdf0aba11b0801a2216e24bf3b138f5748923a
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-11-04 12:36:59 -0500

    modified table schema

--- init_db.py
+++ init_db.py
@@ -13,26 +13,38 @@ cur = conn.cursor()
 cur.execute('DROP TABLE IF EXISTS guitars;')
 cur.execute('CREATE TABLE guitars (id serial PRIMARY KEY,'
                                  'name varchar (150) NOT NULL,'
-                                 'wood_type varchar (50) NOT NULL,'
-                                 'cost integer NOT NULL,'
+                                 'description varchar (500) NOT NULL,'
+                                 'neck_wood varchar (50) NOT NULL,'
+                                 'top_wood varchar (50) NOT NULL,'
+                                 'shape varchar (50) NOT NULL,'
+                                 'back_sides_wood varchar (50) NOT NULL,'
+                                 'construction varchar (150) NOT NULL,'
                                  'date_added date DEFAULT CURRENT_TIMESTAMP);'
                                  )
 
-cur.execute('INSERT INTO guitars (name, wood_type, cost)'
-            'VALUES (%s, %s, %s)',
+cur.execute('INSERT INTO guitars (name, description, neck_wood, top_wood, shape, back_sides_wood, construction)'
+            'VALUES (%s, %s, %s, %s, %s, %s, %s)',
             ('Guitar numero one1',
-             'Canadian walnut',
-             4200)
+             'this guitar was built to be a guitar.  It was successfully made as a guitar',
+             'Ebony colored ebony',
+             'Bohemian Spruce',
+             'Grand Dreadnaught',
+             'Canadian Walnut',
+             'Constructed using standard guitar making tools'
+             )
             )
 
-cur.execute('INSERT INTO guitars (name, wood_type, cost)'
-            'VALUES (%s, %s, %s)',
-            ('Guitar numero notone111nope',
-             'Correllian walnut',
-             99999999)
+cur.execute('INSERT INTO guitars (name, description, neck_wood, top_wood, shape, back_sides_wood, construction)'
+            'VALUES (%s, %s, %s, %s, %s, %s, %s)',
+            ('Guitar numero notone1',
+             'this guitar was built for a child prodigy in western montezuma.',
+             'Ebony colored spruce',
+             'Home Depot Plywood',
+             'Slightly unimpressive Auditorium',
+             'Pine shavings held together by glue',
+             'Constructed using a screwdriver and a box cutter'
+             )
             )
-
-
 
 conn.commit()
 
